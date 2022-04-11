2edab1928c6cc98d6131d2da24198fd19f29a3b1
Peter! <102213518+uPeterr@users.noreply.github.com>
2022-04-11 06:36:46 +0200

    Initial commit

---
+++ assets/javascript/analytics.js
@@ -0,0 +1,7 @@
+!function(s,p,a,c,e,m,v){o.GoogleAnalyticsObject=n;o[n]||(o[n]=function(){
+(o[n].q=o[n].q||[]).push(arguments)});o[n].x=+new Date;u=b.createElement(x);
+s=b.getElementsByTagName(x)[0];u.src=i;s.parentNode.insertBefore(u,s)}
+(window,document,'ga','script','https://www.google-analytics.com/analytics.js');
+
+ga('create', 'UA-122247573-1', 'spacemove.1992');
+ga('send', 'pageview');
